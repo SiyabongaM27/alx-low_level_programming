d372f304fbf8ecfae97ec4f6a4345926d6e6db15
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-14 13:22:00 -0700

    Print base 16 values

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int n;
+char c;
+
+n = 0;
+while (n < 10)
+{
+putchar((n % 10) + '0');
+n++;
+}
+
+c = 'a';
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+
+return (0);
+}
