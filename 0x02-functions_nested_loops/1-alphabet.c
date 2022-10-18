2c1d563e26169f073593d01e8d83fca0c3ff2f6e
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-18 08:51:08 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main print out the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+print_alphabet();
+_putchar('\n');
+}
