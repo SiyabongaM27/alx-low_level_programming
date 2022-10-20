54d3f18546157ab072e7b4a4b31e3a767a7b1cc7
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-20 22:31:59 +0200

    Amendment 2 of fizz buzz - betty

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,21 @@ for (i = 1; i <= 100; i++)
 if ((i % 15) == 0)
 {
 printf("FizzBuzz");
-printf(" ");
 }
 else if ((i % 5) == 0)
 {
 printf("Buzz ");
-printf(" ");
 }
 else if ((i % 3) == 0)
 {
 printf("Fizz ");
-printf(" ");
 }
 else
 {
 printf("%d", i);
+}
+if (i < 100)
+{
 printf(" ");
 }
 }
