56deea20d71403196149047e7835bb1c129460d3
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-20 22:21:51 +0200

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 15) == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz ");
+printf(" ");
+}
+else if ((i % 3) == 0)
+{
+printf("Fizz ");
+printf(" ");
+}
+else
+{
+printf("%d ", i);
+printf(" ");
+}
+}
+printf("\n");
+}
