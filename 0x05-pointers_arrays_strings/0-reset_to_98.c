9268e87726401717dbbad997afa5c82a4ff35d08
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-24 16:54:00 +0200

    Reset value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to 98
+ *
+ * @n: pointer parsed into function;
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
