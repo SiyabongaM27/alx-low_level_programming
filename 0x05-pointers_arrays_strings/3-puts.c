f8a534b192017774ab5c6fb359d06740fadfeb71
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-10-24 18:03:33 +0200

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string
+ *
+ * @str: string parsed to function;
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
