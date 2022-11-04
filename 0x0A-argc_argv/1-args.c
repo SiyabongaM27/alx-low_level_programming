57d9e788a5921eb06f004f1af1e8ede7a984a528
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-04 15:10:57 +0200

    Print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: counter
+ * @argv: arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
