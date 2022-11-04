ff78d49194c3baa8586b94d78e110ae7c3b14f28
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-04 14:57:02 +0200

    print the project name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: counter
+ * @argv: arguments
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
