7e8f64d88f310d61f9dcd888c356f3602399594a
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-08 14:54:39 +0200

    duplicate a string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,45 +12,34 @@
 
 char *_strdup(char *str)
 {
-int i, len;
+int len, i;
 char *p;
 
 if (str == NULL)
 return ('\0');
 
-i = 0;
 len = 0;
 
-while (str[len] != '\0')
+while (*(str + len) != '\0')
 {
 len++;
 }
 
-p = malloc(sizeof(char) * len+1);
+len++;
+
+p = malloc(sizeof(*str) * len);
 
 if (p == NULL)
 return ('\0');
 
-while (*str != '\0')
+i = 0;
+while (i < len)
 {
-*(p+1) = *(str+1)
+p[i] = *(str + i);
+i++;
 }
+
+p[i] = '\0';
 
 return (p);
-free(p);
-}
-
-int main(void)
-{
-    char *s;
-
-    s = _strdup("ALX SE");
-    if (s == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
 }
