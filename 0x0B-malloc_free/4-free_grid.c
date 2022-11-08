9b28408b0042d7617f970790a5b94ba885577dc1
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-08 19:24:07 +0200

    Free my boy grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - free previously allocated memory
+ *
+ * @grid: grid from previous task
+ * @height: height of grid
+ *
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+
+while (i < height)
+{
+free(grid[i]);
+i++
+}
+free(grid);
+}
