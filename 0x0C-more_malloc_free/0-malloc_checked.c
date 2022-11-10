636c8ff0f39ff49645876b5ab52934cc8540a4e9
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-10 15:09:43 +0200

    README.md

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocate memory
+ *
+ * @b: integer parsed
+ *
+ * Return: nothing
+ */
+ 
+void *malloc_checked(unsigned int b)
+{
+
+}
