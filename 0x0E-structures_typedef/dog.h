0f5040e2cce53009e75f0e9cbef2777d2b7925ec
Siyabonga Mdhluli <siyabongamdhluli.sm@gmail.com>
2022-11-16 14:33:15 +0200

    DEfine dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - describes dog struct
+ * @name: name attribute
+ * @age: age attribute
+ * @owner: owner attribute
+ *
+ * Description: describe dog struct
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
